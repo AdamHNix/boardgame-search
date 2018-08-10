2f09dc9256850300d913c797ceb4e02ea68cf7ad
Adam Nix <adam.h.nix@gmail.com>
2018-08-10 18:57:03 -0500

    xml to JSON conversion function added

--- src/components/board-games/board-games.js
+++ src/components/board-games/board-games.js
@@ -1,25 +1,21 @@
 import React from 'react';
 //xml to json coverter
-import xml2js from 'xml2js';
+import xml2js from 'xml2js'
 
-let parseString = xml2js.parseString;
-
+const parseString = xml2js.parseString;
 const Game = ({searchStatus}) =>{
     //fetches BGG xmlapi2
      let getGames = () => {
-         console.log("HI")
+         //convert xml to JSON
         // fetch(`https://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=${searchStatus}`)
-        fetch(`https://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=agricola`)
+        return fetch(`https://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=agricola`)
         .then(response => response.text())
-        //test to see if XML fetch is working
-        .then(data => console.log(data));
+        .then( games2Json =>
+            parseString(games2Json, function (err, result) {
+            console.dir(result);
+            }))
     }
-    //converts BGG xml api to json
-    let games2Json = getGames()
-    parseString(games2Json, function (err, result) {
-        JSON.stringify(result);
-    });
-    console.log(games2Json)
+    console.log(getGames());
     return (
     <div>
         <img alt = "boardgame" src="#" />
