83c80a6802bf6bb5a26235905c42c05e5549b806
Adam Nix <adam.h.nix@gmail.com>
2018-08-19 23:46:49 -0500

    finished search. organized results by popularity. set up error catching for no result searches. set up gh-pages

--- src/App.js
+++ src/App.js
@@ -29,20 +29,23 @@ class App extends Component {
       //convert xml to JSON
       .then(data => {
         let jsonData = parseString(data, (err, result) => {
+          let games = [];
           if (err) {
             console.log("ERROR converting XML!");
           }
           JSON.stringify(result);
-          console.log("result 1", result);
           //sets object to array of lower compoents (items)
+          if (result.items.$.total === "0") {
+            games = ["Sorry, I couldn't find anything..."];
+            this.setState({ games: games });
+            return true;
+          }
           result = Object.values(result.items.item).filter(gametype => {
             return gametype.$.type === "boardgame";
           });
-          console.log("result 2", result);
-          let games = [];
           console.log("total games", result.length);
           // loops through first 10 or less games that come up in fetch
-          for (let game = 0; game <= 20 && game < result.length; game++) {
+          for (let game = 0; game < 50 && game < result.length; game++) {
             let gameObj = Object.values(result[game]);
             console.log("game object", gameObj);
             console.log("current game", game);
@@ -56,45 +59,52 @@ class App extends Component {
             console.log("Finished push");
           }
           //second fetch to obtain all stats on each boardgame
-          Object.values(games).map(obj =>
-            fetch(
-              `https://boardgamegeek.com/xmlapi2/thing?id=${obj.id}&stats=1`
-            )
-              .then(response => response.text())
-              .then(imageData => {
-                parseString(imageData, (err, result) => {
-                  if (err) {
-                    console.log("ERROR converting XML!");
+          games = Object.values(games).map(obj => (obj = obj.id));
+          console.log(games);
+          let stringGames = games.join(",");
+          console.log(stringGames);
+          fetch(
+            `https://boardgamegeek.com/xmlapi2/thing?id=${stringGames}&stats=1`
+          )
+            .then(response => response.text())
+            .then(data => {
+              parseString(data, (err, result) => {
+                if (err) {
+                  console.log("ERROR converting XML!");
+                }
+                JSON.stringify(result);
+                console.log(result);
+                games = Object.values(result.items.item).map(game => {
+                  let type = game.$.type;
+                  if (type !== "boardgame") {
+                    return (game = null);
                   }
-                  JSON.stringify(result);
-                  console.dir(result);
-                  // if((result.items.item[0].$.type) !== "boardgame"){
-                  //   console.log(result.items.item[0].$.type)
-                  //   return games
-                  // }
-                  // else{
-                  console.log(result);
-                  //result = Object.values(result).sort(())
-                  let getPic = result.items.item[0].image[0];
-                  obj.image = getPic;
-                  // }
+                  return game;
                 });
-                this.setState({ games: games });
+                games = Object.values(games).sort((a, b) => {
+                  if (a === null) {
+                    return true;
+                  }
+                  if (b === null) {
+                    return true;
+                  }
+                  let aPop = a.statistics[0].ratings[0].usersrated[0].$.value;
+                  let bPop = b.statistics[0].ratings[0].usersrated[0].$.value;
+                  return bPop - aPop;
+                });
                 console.log(games);
-              })
-          );
-          // not sure where to put this --
-          // games = games.filter(game => {
-          // return game.image.length > 0;
-          // })
+              });
+              this.setState({ games: games });
+              console.log(games);
+            });
           return jsonData;
         });
       });
   };
   //tracks what is in searchbox
-  onSearchChange() {
+  onSearchChange = event => {
     this.setState({ searchfield: event.target.value });
-  }
+  };
   render() {
     return (
       <div className="flex-center">
--- src/components/BoardGameImage/boardGameImage.js
+++ src/components/BoardGameImage/boardGameImage.js
@@ -2,34 +2,43 @@ import React from "react";
 import "./boardGameImage.css";
 
 const BoardGameImage = ({ games }) => {
-  // games.forEach(element => {
-  //     imgAndName.push(element.image);
-  //     imgAndName.push(element.name)
-
-  // });
-  // games.forEach(element =>{
-  //     return name.push(element.name)
-  // })
-  return (
-    <div className="flex-row" id="images">
-      {Object.values(games).map(game => {
-        return (
-          <div className="flex-center games-searched">
-            <img
-              className="shadow-5"
-              key={game}
-              id="game-img"
-              alt={game.name}
-              src={game.image}
-            />
-            <h3 key={game + ".1"} className="flex-center">
-              {game.name}
-            </h3>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (games[0] === "Sorry, I couldn't find anything...") {
+    return (
+      <div id="sorry">
+        <h2 className="flex-center">{games[0]}</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex-row" id="images">
+        {Object.values(games).map(game => {
+          if (game === null) {
+            return true;
+          } else if (game.image === undefined) {
+            game.image = [""];
+          }
+          let link = "https://boardgamegeek.com/boardgame/" + game.$.id;
+          return (
+            <a href={link} target="_blank">
+              <div className="flex-center games-searched">
+                <img
+                  className="shadow-5"
+                  key={game + "image"}
+                  id="game-img"
+                  alt="unavailable"
+                  src={game.image[0]}
+                />
+                <h3 key={game + "name"} className="flex-center">
+                  {game.name[0].$.value}
+                </h3>
+                <h4 className="flex-center">{game.yearpublished[0].$.value}</h4>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    );
+  }
 };
 
 export default BoardGameImage;
